11ed8e408526fb754814285c02940c31015c241c
vladimirmartinvh-sudo <vladimirmartinvh@gmail.com>
2025-10-03 16:22:35 -0500

    Update cxn.js

--- js/cxn.js
+++ js/cxn.js
@@ -7,15 +7,6 @@ import { getFirestore, collection, addDoc } from "https://www.gstatic.com/fireba
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDLIiZDuujN9IvFUdWMzfGAdSCwCZsnjfM",
-    authDomain: "form-portafolio-d6bf9.firebaseapp.com",
-    projectId: "form-portafolio-d6bf9",
-  storageBucket: "form-portafolio-d6bf9.appspot.com",
-    messagingSenderId: "857530147233",
-    appId: "1:857530147233:web:325be86ccfcd92d67d6be0",
-    measurementId: "G-GTWZH3013J"
-  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -49,4 +40,5 @@ form.addEventListener("submit", async (e) => {
         console.log("Error al enviar datos");
         alert("Hubo un error al enviar datos");
     }
+
 });
